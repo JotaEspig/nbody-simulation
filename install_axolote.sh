d88974d5df7422cfec21d74eaf950bdf20d0e256
João Vitor Espig <jotaespig@gmail.com>
2024-04-20 12:15:26 -0300

    removed branch from installation script

--- install_axolote.sh
+++ install_axolote.sh
@@ -1,6 +1,6 @@
 pwd=$(pwd)
 cd /tmp
-git clone -b v0.2.2 https://github.com/JotaEspig/axolote-engine.git
+git clone https://github.com/JotaEspig/axolote-engine.git
 cd axolote-engine
 sed -i 's/\r//' install.sh
 echo -n "1" | ./install.sh $pwd
