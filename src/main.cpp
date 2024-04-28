8abb59baedb2e599bea5684a360a9616a91cdbc3
João Vitor Espig <jotaespig@gmail.com>
2024-04-28 17:58:47 -0300

    add small prompt to choose app mode

--- src/main.cpp
+++ src/main.cpp
@@ -11,13 +11,23 @@ int main(int argc, char **argv) {
     }
 
     std::cout << "N-Body Simulation" << std::endl;
+    int choice = 0;
+    std::cout << "[0] - Real time simulation" << std::endl
+              << "[1] - Bake" << std::endl
+              << "[2] - Render baked file" << std::endl
+              << "> ";
+    std::cin >> choice;
+
     App app{};
     app.set_title("Uepa");
     app.set_width(600);
     app.set_height(600);
     app.set_color(0x10, 0x10, 0x10);
-    // app.main_loop(argv[1]);
-    // app.bake(argv[1]);
-    app.render_loop(argv[1]);
+    if (choice == 1)
+        app.bake(argv[1]);
+    else if (choice == 2)
+        app.render_loop(argv[1]);
+    else
+        app.main_loop(argv[1]);
     return 0;
 }
