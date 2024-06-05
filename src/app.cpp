aaf42d4fd54aa1e96f4a3f036f938301f8179a0d
João Vitor Espig <jotaespig@gmail.com>
2024-06-05 15:02:22 -0300

    feat: baking and rendering working with new system

--- src/app.cpp
+++ src/app.cpp
@@ -78,10 +78,6 @@ void App::main_loop(const char *json_filename) {
 
     std::string original_title = title();
 
-    axolote::gl::Shader shader_program = axolote::gl::Shader(
-        "./resources/shaders/vertex_shader.glsl",
-        "./resources/shaders/fragment_shader.glsl"
-    );
     axolote::gl::Shader instanced_shader_program = axolote::gl::Shader(
         "./resources/shaders/instanced_vertex_shader.glsl",
         "./resources/shaders/fragment_shader.glsl"
@@ -137,90 +133,85 @@ void App::main_loop(const char *json_filename) {
 }
 
 void App::bake(const char *json_filename) {
-    /*
-        using json = nlohmann::json;
-        std::ifstream file(json_filename);
-        json data = json::parse(file);
-        double dt_multiplier = data["dt_multiplier"];
+    using json = nlohmann::json;
+    std::ifstream file(json_filename);
+    json data = json::parse(file);
+    double dt_multiplier = data["dt_multiplier"];
 
-        // The shader is create just because it's needed in setup_using_json
-        axolote::gl::Shader shader_program(
-            "./resources/shaders/vertex_shader.glsl",
-            "./resources/shaders/fragment_shader.glsl"
-        );
-        // Current scene is needed for process input from user
-        current_scene = std::make_shared<axolote::Scene>();
+    // Current scene is needed for process input from user
+    current_scene = std::make_shared<axolote::Scene>();
 
-        // Celestial Body system
-        bodies_system->setup_using_json(shader_program, data);
+    // Celestial Body system
+    bodies_system->setup_using_json(data);
 
-        std::cout << "LET HIM COOK!" << std::endl
-                  << "DO NOT PRESS Ctrl+C" << std::endl;
+    std::cout << "LET HIM COOK!" << std::endl
+              << "DO NOT PRESS Ctrl+C" << std::endl
+              << "IF YOU WANT TO STOP PRESS ESC" << std::endl;
 
-        std::string s = std::string{json_filename} + ".baked";
-        std::ofstream outputfile{s};
-        std::size_t counter = 0;
-        outputfile << "[" << std::endl;
-        while (!should_close()) {
-            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-            glClear(GL_COLOR_BUFFER_BIT);
+    std::string output_filename = std::string{json_filename} + ".baked";
+    std::ofstream outputfile{output_filename};
+    std::size_t counter = 0;
+    outputfile << "[" << std::endl;
+    while (!should_close()) {
+        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
 
-            glfwPollEvents();
+        glfwPollEvents();
 
-            double dt = 1.0 / 60;
-            dt *= dt_multiplier;
+        double dt = 1.0 / 60;
+        dt *= dt_multiplier;
 
-            process_input(dt);
+        process_input(dt);
 
-            bodies_system->update(dt);
+        bodies_system->update(dt);
 
-            int i = 0;
-            int size = bodies_system->celestial_bodies().size();
-            outputfile << "[";
-            for (auto &c : bodies_system->celestial_bodies()) {
-                BodyDataJSON b = {c->mass(), c->pos.x, c->pos.y, c->pos.z};
-                json outputjson = b;
-                outputfile << outputjson;
-                if (i < size - 1) {
-                    outputfile << ",";
-                }
-                ++i;
-            }
-            outputfile << "]";
-
-            ++counter;
-            if (counter % 60 == 0) {
-                std::cout << "Rendered: " << counter / 60
-                          << " seconds --- DO NOT PRESS Ctrl+C" << std::endl;
-            }
-
-            if (!should_close()) {
+        int i = 0;
+        int size = bodies_system->celestial_bodies().size();
+        outputfile << "[";
+        for (auto &c : bodies_system->celestial_bodies()) {
+            BodyDataJSON b = {c->mass(), c->pos.x, c->pos.y, c->pos.z};
+            json outputjson = b;
+            outputfile << outputjson;
+            if (i < size - 1) {
                 outputfile << ",";
             }
-            outputfile << std::endl;
+            ++i;
+        }
+        outputfile << "]";
 
-            glfwSwapBuffers(window);
+        ++counter;
+        if (counter % 60 == 0) {
+            std::cout << "Rendered: " << counter / 60
+                      << " seconds --- DO NOT PRESS Ctrl+C" << std::endl;
         }
 
-        outputfile << "]" << std::endl;
-    */
+        if (!should_close()) {
+            outputfile << ",";
+        }
+        outputfile << std::endl;
+
+        glfwSwapBuffers(window);
+    }
+
+    outputfile << "]" << std::endl;
+    std::cout << "Done!" << std::endl
+              << "Content saved at: " << output_filename << std::endl;
 }
 
 void App::render_loop(const char *json_filename) {
-    /*
     using json = nlohmann::json;
 
     pause = true;
     std::string original_title = title();
 
-    axolote::gl::Shader shader_program(
-        "./resources/shaders/vertex_shader.glsl",
+    axolote::gl::Shader instanced_shader_program(
+        "./resources/shaders/instanced_vertex_shader.glsl",
         "./resources/shaders/fragment_shader.glsl"
     );
 
-    shader_program.activate();
-    shader_program.set_uniform_int("light.is_set", 0);
-    bodies_system->shader = shader_program;
+    bool first_iteration = true;
+    bool first_setup = true;
+    bodies_system->bind_shader(instanced_shader_program);
 
     // Scene object
     current_scene = std::make_shared<axolote::Scene>();
@@ -232,6 +223,8 @@ void App::render_loop(const char *json_filename) {
     current_scene->camera.speed = 80.0f;
     current_scene->camera.sensitivity = 10000.0f;
     current_scene->camera.max_dist = 1000.0f;
+
+    current_scene->add_drawable(bodies_system);
 
     std::ifstream file(json_filename);
     std::string line;
@@ -256,6 +249,9 @@ void App::render_loop(const char *json_filename) {
         if (!pause) {
             std::getline(file, line);
             if (line == "]") {
+                std::cout
+                    << "Rendered all frames, press Ctrl+C, ESC or P to exit"
+                    << std::endl;
                 pause = true;
                 continue;
             }
@@ -265,16 +261,17 @@ void App::render_loop(const char *json_filename) {
 
             json j = json::parse(line);
 
-            auto scene = std::make_shared<axolote::Scene>();
-            scene->camera = current_scene->camera;
-
-            bodies_system->setup_using_baked_frame_json(shader_program, j);
-            for (auto &e : bodies_system->celestial_bodies()) {
-                scene->add_drawable(e);
+            bodies_system->setup_using_baked_frame_json(j);
+            if (first_setup) {
+                bodies_system->setup_instanced_vbo();
+                first_setup = false;
             }
 
-            current_scene = scene;
             current_scene->update(dt);
+        }
+        else if (first_iteration) {
+            std::cout << "Press P to start" << std::endl;
+            first_iteration = false;
         }
 
         current_scene->update_camera((float)width() / height());
@@ -282,5 +279,4 @@ void App::render_loop(const char *json_filename) {
 
         glfwSwapBuffers(window);
     }
-*/
 }
