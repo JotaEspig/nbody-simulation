b95b6670d67c2816979d5789c8d43c13b1e49791
João Vitor Espig <jotaespig@gmail.com>
2024-09-11 23:25:17 -0300

    fix small things

--- install_axolote.sh
+++ install_axolote.sh
@@ -1,6 +1,5 @@
 pwd=$(pwd)
-cd /tmp
-git clone -b v0.7.1 https://github.com/JotaEspig/axolote-engine.git
+git clone -b v0.7.1 https://github.com/JotaEspig/axolote-engine.git --depth=1
 cd axolote-engine
 sed -i 's/\r//' install.sh
 echo -n "1" | ./install.sh $pwd
